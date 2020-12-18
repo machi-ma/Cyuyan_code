8c38fbf17841645a868705bfd3208b8b601e07f9
machi-ma <2957899957@99.com>
2020-12-18 18:09:30 +0800

    C语言循环练习代码

---
+++ xunhuan_code/2019.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+	while (i <= 100)
+	{
+		printf("%d", i);
+	}
+	return 0;
+}
+//int main()
+//{
+//	int a, b, c, ret;
+//	scanf("%d %d %d", &a, &b, &c);
+//	
+//		if (a < b)
+//		{
+//			ret = a;
+//			a = b;
+//			b = ret;
+//		}
+//		if (a < c)
+//		{
+//			ret = a;
+//			a= c;
+//			c = ret;
+//		}
+//		if (b<c)
+//		{
+//			ret = b;
+//			b=c;
+//			c=ret;
+//		}
+//	printf("%d %d %d", a, b, c);
+//	return 0;
+//}
